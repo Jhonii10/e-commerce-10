3cdb61b0576e97c02f1cb87d1ead778b34cd0502
Jhonni10 <jhonieripianoscue@gmail.com>
2024-01-26 08:43:07 -0500

    new component in page home - slider.js

---
+++ src/components/slider/sliderDate.js
@@ -0,0 +1,22 @@
+export const SLIDERDATE = [
+    {
+        image: "https://i.ibb.co/CBGRLhG/bg-4.jpg",
+        heading: "Villa de Zapatos",
+        desc: "Hasta un 30% de descuento en todos los productos en oferta.",
+    },
+    {
+        image: "https://i.ibb.co/cDLBk5h/bg-1.jpg",
+        heading: "Moda para Mujeres",
+        desc: "Hasta un 30% de descuento en todos los productos en oferta.",
+    },
+    {
+        image: "https://i.ibb.co/HXjD3V0/bg-2.jpg",
+        heading: "Moda para Hombres",
+        desc: "Hasta un 30% de descuento en todos los productos en oferta.",
+    },
+    {
+        image: "https://i.ibb.co/H2FRmtV/bg-3.jpg",
+        heading: "Increíbles Gadgets",
+        desc: "Hasta un 30% de descuento en todos los productos en oferta.",
+    },  
+];
