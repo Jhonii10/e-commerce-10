af1e4b3f2913f240fc3a231b6a173b87bbaac48a
Jhonni10 <jhonieripianoscue@gmail.com>
2024-02-14 16:49:36 -0500

    new functionalities in cart.js with redux

--- src/redux/slice/cartSlice.js
+++ src/redux/slice/cartSlice.js
@@ -18,7 +18,6 @@ export const cartSlice = createSlice({
 
         if (productIndex >= 0) {
             state.cartItems[productIndex].cartQuantity += 1;
-            toast.info(`${payload.name} incrementado una unidad `)
         } else {
             const tempProducts = {...payload, cartQuantity:1}
             state.cartItems.push(tempProducts);
@@ -27,9 +26,40 @@ export const cartSlice = createSlice({
 
         //Agregar al Local Storage
         localStorage.setItem("cart-items",JSON.stringify(state.cartItems))
+   },
+   DECREASE_CART:(state, {payload})=>{
+    const productIndex = state.cartItems.findIndex(
+        (item)=> item.id === payload.id)
+    
+        if (state.cartItems[productIndex].cartQuantity > 1) {
+            state.cartItems[productIndex].cartQuantity -= 1;
+        } else if (state.cartItems[productIndex].cartQuantity === 1 ){
+            const newCartItem = state.cartItems.filter((item)=>item.id !== payload.id)
+            state.cartItems = newCartItem
+        }
+
+        localStorage.setItem("cart-items", JSON.stringify(state.cartItems));
+
+   },
+   REMOVE_FROM_CART: (state, {payload}) => {
+     
+     const newCartItem = state.cartItems.filter((item)=>item.id !== payload.id)
+     
+     state.cartItems = newCartItem
+
+     localStorage.setItem("cart-items", JSON.stringify(state.cartItems));
+
+   },
+   CLEAR_CART:(state)=>{
+    
+        state.cartItems = [];
+        state.cartTotalQuantity = 0;
+        state.cartTotalAmount = 0;
+
+        localStorage.removeItem('cart-items')
    }
   }
 })
 
 // Action creators are generated for each case reducer function
-export const { ADD_TO_CART } = cartSlice.actions
+export const { ADD_TO_CART, DECREASE_CART, REMOVE_FROM_CART , CLEAR_CART } = cartSlice.actions
