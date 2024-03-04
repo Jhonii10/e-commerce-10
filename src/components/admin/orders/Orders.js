77d8917a6d24e860447ac7c5a037e76e27f150fe
Jhonni10 <jhonieripianoscue@gmail.com>
2024-03-04 18:51:08 -0500

    new page in pages de admin - all orders

--- src/components/admin/orders/Orders.js
+++ src/components/admin/orders/Orders.js
@@ -1,10 +1,92 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { OrderContainer } from './OrdersContainer';
+import { useNavigate } from 'react-router-dom';
+
+import { UseFetchCollection } from '../../../hooks/useFetchCollection';
+import { useDispatch, useSelector } from 'react-redux';
+import { STORE_ORDERS } from '../../../redux/slice/orderSlice';
+import Loader from '../../loader/Loader';
+import { UseFormatAmount } from '../../../hooks/useFormatAmount';
 
 const Orders = () => {
+   
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const {data ,  isLoading } = UseFetchCollection('orders');
+    const orders = useSelector((state)=>state.orders.orderHistory);
+    const {formatAmount} = UseFormatAmount();
+
+    const handleClik = (id) => {
+        return navigate(`/admin/order-details/${id}`); 
+    }
+
+    useEffect(() => {
+        dispatch(STORE_ORDERS(data))
+    }, [data, dispatch]);
+
     return (
-        <div>
-            Orders
-        </div>
+        <OrderContainer>
+            <div className='container'> 
+                <h2>Todos los pedidos</h2>
+                <p>Abre un pedido para <b>cambiar el estado del pedido.</b></p>
+                <br/>
+                <>
+                    {
+                        isLoading && <Loader/>  
+                    }
+                    <div className='table'>
+                      {
+                        orders.length === 0 
+                        ? (
+                            <p>No hay Pedidos</p>
+                          )
+                        :(
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Item</th>
+                                        <th>Fecha</th>
+                                        <th>ID</th>
+                                        <th>Precio</th>
+                                        <th>Estado</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        orders.map((order, i) =>{
+                                            const {id, orderDate , orderTime, orderAmount , orderStatus }= order;
+                                            return(
+                                                <tr key={i} onClick={()=>handleClik(id)}>
+                                                   <td><b>{i+1}</b></td>
+                                                   <td>{orderDate} {orderTime}</td>
+                                                   <td>{id}</td>
+                                                   <td>$ {formatAmount(orderAmount)}</td>
+                                                   <td>
+                                                        <span
+                                                            className={`badge ${orderStatus !== 'Entregado'
+                                                            ? ' pending':' delivered' }`}
+                                                            >
+                                                            {orderStatus}
+                                                        </span>
+                                                   </td>
+                                                   
+
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        )
+                      }
+                    </div>
+                </>
+
+            </div>
+            
+        </OrderContainer>
+        
     );
 }
 
