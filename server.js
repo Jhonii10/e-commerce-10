23471b933bbf80824194c468ac6866f26d2f869a
Jhonni10 <jhonieripianoscue@gmail.com>
2024-03-20 12:28:13 -0500

    config of project for production v.2

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ require('dotenv').config();
 const express = require("express");
 const cors = require('cors')
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
-const helmet = require('helmet');
 
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(helmet());
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
